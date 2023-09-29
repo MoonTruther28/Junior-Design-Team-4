8909ded1957bc09eb034ef0cfc0288c03cc90678
ykulkarni30 <ykulkarni30@gatech.edu>
2023-09-29 13:53:23 -0400

    Sound and LCD changes

--- AudioVisual.py
+++ AudioVisual.py
@@ -1,11 +1,17 @@
 class AudioVisual: # Yash's test change
+    def initialize_display(self):
+        I2C_LCD1602.lcd_init(39)
+
     def display_question(self, planet: str):
         """Method to display the question.
 
         Args:
         planet (str): The planet name in the question.
         """
-        pass
+        if (planet == "Mercury"):
+            I2C_LCD1602.show_string("What planet is", 0, 0)
+            I2C_LCD1602.show_string("closest to sun?", 0, 1)
+            
 
     def display_success(self, planet: str):
         """Method to display a success message.
@@ -25,7 +31,38 @@ class AudioVisual: # Yash's test change
 
     def play_audio(self):
         """Method to play audio."""
-        pass
+        music.play(music.tone_playable(392, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(587, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(784, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(440, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(784, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(587, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(494, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(523, music.beat(BeatFraction.QUARTER)),
+            music.PlaybackMode.UNTIL_DONE)
+        music.play(music.tone_playable(440, music.beat(BeatFraction.HALF)),
+            music.PlaybackMode.UNTIL_DONE)
 
     def stop_audio(self):
         """Method to stop the audio."""
